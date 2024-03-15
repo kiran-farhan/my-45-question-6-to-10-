b32e70c533c79f693938189a4c55032d5cccbf75
kiran-farhan <159946236+kiran-farhan@users.noreply.github.com>
2024-03-15 23:41:20 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,35 @@
+// qestion num: 06
+// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+let personname: string="\n\t kiran_farhan \n\t";
+console.log(personname)
+
+// question num:7and 8
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+// You should create four lines that look like this:
+
+// console.log(5 + 3)
+
+
+console.log(5+3);
+console.log(10-2);
+console.log(4*2);
+console.log(16/2);
+
+//  question num: 9
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favoritenumber: number=20
+console.log( `my favourit number is  ${favoritenumber}`);
+ 
+// question num:10
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// let myname: string= 'kiran_farhan';
+// let date: number= 15/3/2024 
+console.log(`my name is kiran_farhan`)
+
+// name : ayesha
+// date: 15-3-2024
+console.log(`5-3`)
